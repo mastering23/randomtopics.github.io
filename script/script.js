6fbeb9ce2dcd2463d037853be91e3fad0102e943
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-08 01:16:32 -0500

    adding script.js also adding logic to script file

---
+++ script/script.js
@@ -0,0 +1,31 @@
+
+
+const listTopics =[
+"Talk about favorite sport !",
+"Talk about favorite ice cream !",
+"Talk about favorite song !",
+"Talk about favorite food !",
+"Talk about spaces !",
+"Talk about coding !",
+"Talk about comedy show !",
+"Talk about favorite restaurant !",
+"Talk about traveling !",
+"Talk about favorite dance !",
+"Talk about science !",
+"Talk about happy memories !",
+"Talk about futures goals",
+"Talk about hiking !",
+"Talk about art !",
+"Talk about life experiences",
+"Talk about economic ! ",
+"Talk about tech !",
+"Talk about AI !",
+"Talk about clothes !",
+"Talk about pets !",
+"Talk about books!",
+"Talk about Hobbies !",
+"Talk about social media !",
+"Talk about robotics !"
+];
+let topic = listTopics[7];
+console.log(topic);
